a851613fe3ac724b68e584d995e8b60b2abce8cc
hanxue <hanxue@cmhi.chinamobile.com>
2022-09-16 18:30:53 +0800

    Opt 添加一些

--- demo222/src/views/HomeView.vue
+++ demo222/src/views/HomeView.vue
@@ -1,3 +1,19 @@
+<!-- vuedraggable使用
+group {name pull put}
+
+@start 开始拖动时触发
+@add 从一个数组拖拽到另外一个数组时触发的事件
+@remove 移除
+@update 拖拽变换位置时触发的事件
+@choose 鼠标点击选中要拖拽元素时的事件
+@unchoose 选中后松开鼠标的事件
+@sort 位置变化时的事件
+@clone 
+@move 某个元素不允许拖拽和获取当前拖动元素的对象 
+@end 拖拽事件结束，用来判断是否存在对象 -->
+
+<!-- vuedraggable中文文档 -->
+
 <template>
   <div class="home">
     <!-- 左边 组件列表 -->
@@ -14,7 +30,20 @@
       </draggable>
     </div>
     <!-- 中间 画布 -->
-    <div class="mid">222</div>
+    <div class="mid">
+      222
+
+      <draggable v-model="arr2" item-key="name" animation="300">
+        <!-- <transition-group> -->
+        <!-- <div v-for="item in arr2" :key="item.id">
+            {{ item.name }}
+          </div> -->
+        <template #item="{ item }">
+          <div class="list-group-item">{{ item.name }}</div>
+        </template>
+        <!-- </transition-group> -->
+      </draggable>
+    </div>
     <!-- 右边 编辑组件 -->
     <div class="right">333</div>
   </div>
@@ -27,6 +56,7 @@ import draggable from "vuedraggable";
 export default defineComponent({
   components: { draggable },
   setup() {
+    const arr2 = ref([]);
     const controlList = ref([
       { name: "单选题", type: "radio", icon: "ios-radio-button-off" },
       { name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
@@ -42,6 +72,7 @@ export default defineComponent({
 
     return {
       controlList,
+      arr2,
     };
   },
 });
@@ -54,10 +85,20 @@ export default defineComponent({
 }
 .left {
   width: 30%;
-  background: lightblue;
+  background: white;
 }
 .list-group-item {
   padding: 10px;
+  border: double;
+}
+.mid {
+  width: 40%;
+  height: 500px;
+  border: double;
+}
+.right {
+  width: 30%;
+  height: 500px;
   border: double;
 }
 </style>
