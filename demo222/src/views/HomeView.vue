2f31bd09bc8bd0054d287b1d88dadb73c4c27755
hanxue <hanxue@cmhi.chinamobile.com>
2022-09-16 17:35:12 +0800

    Opt 可以拖动了

--- demo222/src/views/HomeView.vue
+++ demo222/src/views/HomeView.vue
@@ -2,21 +2,15 @@
   <div class="home">
     <!-- 左边 组件列表 -->
     <div class="left">
-      111
       <draggable
-        :sort="false"
-        list="controlList"
-        :group="{ name: 'fields', pull: 'clone', put: false }"
-        :clone="cloneField"
+        v-model="controlList"
+        item-key="name"
+        @start="drag = true"
+        @end="drag = false"
       >
-        <div
-          v-for="(element, index) in controlList"
-          :key="index"
-          class="list-group-item"
-        >
-          <Icon :type="element.icon" :size="20" class="list-group-item-icon" />
-          {{ element.name }}
-        </div>
+        <template #item="{ element }">
+          <div class="list-group-item">{{ element.name }}</div>
+        </template>
       </draggable>
     </div>
     <!-- 中间 画布 -->
@@ -26,31 +20,31 @@
   </div>
 </template>
 
-<script setup lang="ts">
-// import { defineComponent } from "vue";
+<script>
+import { defineComponent, ref } from "vue";
+import draggable from "vuedraggable";
 
-// export default defineComponent({
-//   name: "HomeView",
-//   components: {},
-// });
-// interface one {
-//   name: string;
-//   type: string;
-//   icon: string;
-// }
+export default defineComponent({
+  components: { draggable },
+  setup() {
+    const controlList = ref([
+      { name: "单选题", type: "radio", icon: "ios-radio-button-off" },
+      { name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
+      { name: "填空题", type: "text", icon: "ios-create-outline" },
+      { name: "日期时间", type: "date", icon: "ios-calendar-outline" },
+      { name: "手机号码", type: "mobile", icon: "ios-phone-portrait" },
+      { name: "邮箱", type: "email", icon: "ios-mail-outline" },
+      { name: "地区", type: "address", icon: "ios-locate-outline" },
+      { name: "性别", type: "gender", icon: "ios-male" },
+      { name: "图片", type: "imageUploader", icon: "ios-images-outline" },
+      { name: "定位", type: "position", icon: "ios-pin-outline" },
+    ]);
 
-let controlList: any[] = [
-  { name: "单选题", type: "radio", icon: "ios-radio-button-off" },
-  { name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
-  { name: "填空题", type: "text", icon: "ios-create-outline" },
-  { name: "日期时间", type: "date", icon: "ios-calendar-outline" },
-  { name: "手机号码", type: "mobile", icon: "ios-phone-portrait" },
-  { name: "邮箱", type: "email", icon: "ios-mail-outline" },
-  { name: "地区", type: "address", icon: "ios-locate-outline" },
-  { name: "性别", type: "gender", icon: "ios-male" },
-  { name: "图片", type: "imageUploader", icon: "ios-images-outline" },
-  { name: "定位", type: "position", icon: "ios-pin-outline" },
-];
+    return {
+      controlList,
+    };
+  },
+});
 </script>
 
 <style>
