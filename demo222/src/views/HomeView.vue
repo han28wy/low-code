eaab9f685080c6fbf449c01deeeed7a5280cd766
hanxue <hanxue@cmhi.chinamobile.com>
2022-09-16 15:40:33 +0800

    Opt 新增控件

--- demo222/src/views/HomeView.vue
+++ demo222/src/views/HomeView.vue
@@ -1,18 +1,69 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- 左边 组件列表 -->
+    <div class="left">
+      111
+      <draggable
+        :sort="false"
+        list="controlList"
+        :group="{ name: 'fields', pull: 'clone', put: false }"
+        :clone="cloneField"
+      >
+        <div
+          v-for="(element, index) in controlList"
+          :key="index"
+          class="list-group-item"
+        >
+          <Icon :type="element.icon" :size="20" class="list-group-item-icon" />
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
+    <!-- 中间 画布 -->
+    <div class="mid">222</div>
+    <!-- 右边 编辑组件 -->
+    <div class="right">333</div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+<script setup lang="ts">
+// import { defineComponent } from "vue";
 
-export default defineComponent({
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-});
+// export default defineComponent({
+//   name: "HomeView",
+//   components: {},
+// });
+// interface one {
+//   name: string;
+//   type: string;
+//   icon: string;
+// }
+
+let controlList: any[] = [
+  { name: "单选题", type: "radio", icon: "ios-radio-button-off" },
+  { name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
+  { name: "填空题", type: "text", icon: "ios-create-outline" },
+  { name: "日期时间", type: "date", icon: "ios-calendar-outline" },
+  { name: "手机号码", type: "mobile", icon: "ios-phone-portrait" },
+  { name: "邮箱", type: "email", icon: "ios-mail-outline" },
+  { name: "地区", type: "address", icon: "ios-locate-outline" },
+  { name: "性别", type: "gender", icon: "ios-male" },
+  { name: "图片", type: "imageUploader", icon: "ios-images-outline" },
+  { name: "定位", type: "position", icon: "ios-pin-outline" },
+];
 </script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: row;
+}
+.left {
+  width: 30%;
+  background: lightblue;
+}
+.list-group-item {
+  padding: 10px;
+  border: double;
+}
+</style>
