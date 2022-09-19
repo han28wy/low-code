379278d603493660b1bbd476673a2151ed79bb0f
hanxue <hanxue@cmhi.chinamobile.com>
2022-09-19 16:11:15 +0800

    Opt 实现左右拖拽

--- demo222/src/views/HomeView.vue
+++ demo222/src/views/HomeView.vue
@@ -19,62 +19,125 @@ group {name pull put}
     <!-- 左边 组件列表 -->
     <div class="left">
       <draggable
-        v-model="controlList"
-        item-key="name"
-        @start="drag = true"
-        @end="drag = false"
+        :list="controlList"
+        item-key="id"
+        group="itxst"
+        ghost-class="ghost"
+        handle=".move"
+        filter=".forbid"
+        :force-fallback="true"
+        chosen-class="chosenClass"
+        :fallback-class="true"
+        :fallback-on-body="true"
+        :touch-start-threshold="50"
+        :fallback-tolerance="50"
       >
         <template #item="{ element }">
-          <div class="list-group-item">{{ element.name }}</div>
+          <div class="item move">
+            <div class="move">{{ element.name }}</div>
+          </div>
         </template>
       </draggable>
     </div>
     <!-- 中间 画布 -->
-    <div class="mid">
-      222
-
-      <draggable v-model="arr2" item-key="name" animation="300">
-        <!-- <transition-group> -->
-        <!-- <div v-for="item in arr2" :key="item.id">
-            {{ item.name }}
-          </div> -->
-        <template #item="{ item }">
-          <div class="list-group-item">{{ item.name }}</div>
+    <div class="left">
+      <draggable
+        :list="arr2"
+        item-key="id"
+        group="itxst"
+        ghost-class="ghost"
+        handle=".move"
+        filter=".forbid"
+        :force-fallback="true"
+        chosen-class="chosenClass"
+        animation="300"
+        :fallback-class="true"
+        :fallback-on-body="true"
+        :touch-start-threshold="50"
+        :fallback-tolerance="50"
+        @start="onStart"
+        @end="onEnd"
+      >
+        <template #item="{ element }">
+          <div class="item move">
+            <div class="move">{{ element.name }}</div>
+          </div>
         </template>
-        <!-- </transition-group> -->
       </draggable>
     </div>
     <!-- 右边 编辑组件 -->
-    <div class="right">333</div>
+    <div class="right">
+      333
+      <div class="left">
+        <draggable
+          :list="arr3"
+          item-key="id"
+          group="fields"
+          ghost-class="ghost"
+          :force-fallback="true"
+          chosen-class="chosenClass"
+          animation="300"
+          :fallback-class="true"
+          :fallback-on-body="true"
+          :touch-start-threshold="50"
+          :fallback-tolerance="50"
+          @start="onStart"
+          @end="onEnd"
+        >
+          <template #item="{ element }">
+            <div class="item move">
+              <div class="move">{{ element.name }}</div>
+            </div>
+          </template>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-import { defineComponent, ref } from "vue";
+<script setup>
+import { ref, reactive, watch } from "vue";
 import draggable from "vuedraggable";
 
-export default defineComponent({
-  components: { draggable },
-  setup() {
-    const arr2 = ref([]);
-    const controlList = ref([
-      { name: "单选题", type: "radio", icon: "ios-radio-button-off" },
-      { name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
-      { name: "填空题", type: "text", icon: "ios-create-outline" },
-      { name: "日期时间", type: "date", icon: "ios-calendar-outline" },
-      { name: "手机号码", type: "mobile", icon: "ios-phone-portrait" },
-      { name: "邮箱", type: "email", icon: "ios-mail-outline" },
-      { name: "地区", type: "address", icon: "ios-locate-outline" },
-      { name: "性别", type: "gender", icon: "ios-male" },
-      { name: "图片", type: "imageUploader", icon: "ios-images-outline" },
-      { name: "定位", type: "position", icon: "ios-pin-outline" },
-    ]);
+//拖拽开始的事件
+const onStart = () => {
+  console.log("开始拖拽qqqqqq");
+};
 
-    return {
-      controlList,
-      arr2,
-    };
+//拖拽结束的事件
+const onEnd = () => {
+  console.log("结束拖拽eeeee");
+};
+
+const onMove = (e, originalEvent) => {
+  //不允许停靠
+  if (e.relatedContext.element.disabledPark == true) return false;
+
+  return true;
+};
+var arr2 = reactive([
+  { id: 11, name: "dfsdfgs", type: "radio", icon: "ios-radio-button-off" },
+]);
+var arr3 = [];
+var controlList = reactive([
+  { id: 1, name: "单选题", type: "radio", icon: "ios-radio-button-off" },
+  { id: 2, name: "多选题", type: "checkbox", icon: "ios-checkbox-outline" },
+  { id: 3, name: "填空题", type: "text", icon: "ios-create-outline" },
+  { id: 4, name: "日期时间", type: "date", icon: "ios-calendar-outline" },
+  { id: 5, name: "手机号码", type: "mobile", icon: "ios-phone-portrait" },
+  { id: 6, name: "邮箱", type: "email", icon: "ios-mail-outline" },
+  { id: 7, name: "地区", type: "address", icon: "ios-locate-outline" },
+  { id: 8, name: "性别", type: "gender", icon: "ios-male" },
+  {
+    id: 9,
+    name: "图片",
+    type: "imageUploader",
+    icon: "ios-images-outline",
   },
+  { id: 10, name: "定位", type: "position", icon: "ios-pin-outline" },
+]);
+watch(arr2, (newValue, oldValue) => {
+  console.log("watch 已触发", newValue, "   555 ", oldValue);
 });
 </script>
 
@@ -84,21 +147,39 @@ export default defineComponent({
   flex-direction: row;
 }
 .left {
-  width: 30%;
-  background: white;
+  width: 40%;
+  flex: 1;
+  padding: 10px;
+  border: solid 1px #eee;
+  border-radius: 5px;
+  float: left;
 }
 .list-group-item {
   padding: 10px;
   border: double;
 }
 .mid {
-  width: 40%;
+  width: 60%;
+  flex: 1;
+  padding: 10px;
+  border: solid 1px #eee;
+  border-radius: 5px;
+  float: left;
+}
+.right {
+  width: 20%;
   height: 500px;
   border: double;
 }
-.right {
-  width: 30%;
-  height: 500px;
-  border: double;
+.group {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-content: center;
+  width: 32%;
+  margin-right: 20px;
+}
+.ghost {
+  border: solid 1px rgb(19, 41, 239) !important;
 }
 </style>
