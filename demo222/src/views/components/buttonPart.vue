d83b086e2013950709a0f9ad4d3132a7c95986cc
hanxue <hanxue@cmhi.chinamobile.com>
2022-09-21 18:09:05 +0800

    Opt 按钮名称同步修改

--- demo222/src/views/components/buttonPart.vue
+++ demo222/src/views/components/buttonPart.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
-    <el-button type="primary">Primary</el-button>
+    <el-button type="primary">{{ button.name }}</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { watch, defineProps, defineExpose } from "vue";
+import { watch, defineProps, reactive, defineExpose } from "vue";
 // const props = defineProps(["config"]);
+var button = reactive({
+  name: "我是按钮！",
+});
 
 const resetData = (val) => {
   console.log("我在reset button 中间的数据 ", "      ", val);
+  button.name = val.name;
 };
 defineExpose({ resetData });
 </script>
